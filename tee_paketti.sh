6ff16b77471003fc068847af49613ba9d4272854
mpastell (Dr. ) <matti.pastell@helsinki.fi>
2009-03-31 05:04:47 +0000

    Added vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/animal@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tee_paketti.sh
+++ tee_paketti.sh
@@ -2,12 +2,14 @@
 R --vanilla < Ani_funktiot.R
 cp DESCRIPTION Animal/
 mkdir Animal/inst
+mkdir Animal/inst/doc
 cp CITATION Animal/inst
 rm Animal/Read-and-delete-me
 rm Animal/man/*
 cp man/* Animal/man
+cp vignette/Animal.Rnw vignette/calf1.bh1 Animal/inst/doc
 R CMD build Animal
-R CMD INSTALL Animal_1.01.tar.gz 
-R CMD check Animal_1.01.tar.gz
+R CMD INSTALL Animal_1.02.tar.gz 
+R CMD check Animal_1.02.tar.gz
 R CMD Rd2dvi --pdf Animal
 cp -r Animal pkg
